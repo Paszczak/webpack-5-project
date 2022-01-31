c05771472ca66d88db3cc9cdd0e6dfdd9f37a0b2
Paszczak <szymonwal@gmail.com>
2022-01-31 13:30:38 +0100

    CSS webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require('path');
 
+// Plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 // Setting mode for production in build script!!!
 let mode = process.env.NODE_ENV === "production" ? "production" : "development";
+let target = process.env.NODE_ENV === "production" ? "browserslist" : "web";
 
 module.exports = {
   mode: mode,
   devtool: "source-map",
+  target: target,
+
+  //Plugins
+  plugins: [new MiniCssExtractPlugin()],
+
+  // Webpack reads right to left!!!
   module: {
     rules: [
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -17,10 +31,12 @@ module.exports = {
       }
     ]
   },
+
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
-    port: 3000,
+    port: 3000, // Specifies a port number to listen for requests on
+    hot: true, // Enables webpack's Hot Module Replacement feature
   }
 }
